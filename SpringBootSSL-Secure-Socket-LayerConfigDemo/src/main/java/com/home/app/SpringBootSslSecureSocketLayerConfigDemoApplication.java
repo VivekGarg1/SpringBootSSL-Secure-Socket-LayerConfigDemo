31a00e680bf18c5e0df2b5c41a6f761caf3c1d56
[VivekGarg1] <[vivekgarg020@gmail.com]>
2020-09-05 18:33:29 +0530

    code merge

---
+++ SpringBootSSL-Secure-Socket-LayerConfigDemo/src/main/java/com/home/app/SpringBootSslSecureSocketLayerConfigDemoApplication.java
@@ -0,0 +1,13 @@
+package com.home.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSslSecureSocketLayerConfigDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSslSecureSocketLayerConfigDemoApplication.class, args);
+	}
+
+}
